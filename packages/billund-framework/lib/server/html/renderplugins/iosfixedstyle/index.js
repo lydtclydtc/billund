d7da5e06cb2a734547bbcfb87fc10b9a04ec89d2
robinleej <robinin1@163.com>
2017-12-17 18:05:30 +0800

    add render-plugin sort and extract

--- packages/billund-framework/lib/server/html/renderplugins/iosfixedstyle/index.js
+++ packages/billund-framework/lib/server/html/renderplugins/iosfixedstyle/index.js
@@ -1,16 +1,24 @@
 'use strict';
 
-module.exports = function*() {
+module.exports = function*(config, context) {
+    const ua = context.req.headers['user-agent'];
+    const isIos = !!(ua.match(/(ipad|iphone|ipod).*os\s([\d_]+)/i));
+    if (!isIos) {
+        return {
+            result: ''
+        };
+    }
+
     /*
-    	在ios情况下,可能有些全局click不可用，是因为没有给body增加cursor: pointer;
+        在ios情况下,可能有些全局click不可用，是因为没有给body增加cursor: pointer;
      */
     return {
         result: `
-        		<style>
-        			body {
-    					-webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    					cursor: pointer;
-					}
-				</style>`
+                <style>
+                    body {
+                        -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+                        cursor: pointer;
+                    }
+                </style>`
     };
 };
