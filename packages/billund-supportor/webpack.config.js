5c187f39cadbbc1c7dba72230f6910ad13d96596
robinleej <robinin1@163.com>
2018-01-04 18:54:44 +0800

    make bootstrap useful

--- packages/billund-supportor/webpack.config.js
+++ packages/billund-supportor/webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
     module: {
         rules: [{
             test: /\.(js)$/,
-            exclude: /node_modules\/(?!(billund-utils))/,
+            exclude: /node_modules\/(?!(billund-enums))/,
             use: [{
                 loader: 'babel-loader',
                 options: {
